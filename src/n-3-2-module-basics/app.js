355631f63fedc67172c950de34170bcb26eee7ac
oldorange <jhong257@uwo.ca>
2018-07-17 21:04:50 -0400

    save

---
+++ src/n-3-2-module-basics/app.js
@@ -0,0 +1,14 @@
+console.log('Starting app.');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+console.log(user);
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err){
+    if (err) {
+        console.log('Unable to write the file');
+    }
+});
