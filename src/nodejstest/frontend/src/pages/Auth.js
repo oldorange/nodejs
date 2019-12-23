3fb07136c430a0e74cdc183a523e31adcc931f86
oldorange <jhong257@uwo.ca>
2019-12-22 21:51:16 -0500

    auth page

--- src/nodejstest/frontend/src/pages/Auth.js
+++ src/nodejstest/frontend/src/pages/Auth.js
@@ -1,10 +1,84 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Auth.css';
 const AuthPage = props => {
+
+    const [isLogin, setIsLogin] = useState(true);
+
+    const emailEl = React.createRef();
+
+    const passwordEl = React.createRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const email = emailEl.current.value;
+        const password = passwordEl.current.value;
+    
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            console.log('invalid input');
+          return;
+        }
+    
+        let requestBody = {
+          query: `
+            query {
+              login(email: "${email}", password: "${password}") {
+                userId
+                token
+                tokenExpiration
+              }
+            }
+          `
+        };
+    
+        if (!this.state.isLogin) {
+          requestBody = {
+            query: `
+              mutation {
+                createUser(userInput: {email: "${email}", password: "${password}"}) {
+                  _id
+                  email
+                }
+              }
+            `
+          };
+        }
+    
+        fetch('http://localhost:8000/graphql', {
+          method: 'POST',
+          body: JSON.stringify(requestBody),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(res => {
+            if (res.status !== 200 && res.status !== 201) {
+              throw new Error('Failed!');
+            }
+            return res.json();
+          })
+          .then(resData => {
+            console.log(resData);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      };
+
     return (
-        <form>
-            <div className="">
-                <h1>AuthPage</h1>
+        <form className="auth-form" onSubmit={submitHandler}>
+            <div className="form-control">
+                <label htmlFor="email">E-Mail</label>
+                <input type="email" id="email" ref={emailEl} />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" ref={passwordEl} />
+            </div>
+            <div className="form-actions">
+                <button type="submit">Submit</button>
+                <button type="button" onClick={() => setIsLogin(!isLogin)}>
+                    Switch to {isLogin ? 'Signup' : 'Login'}
+                </button>
             </div>
         </form>
     );
